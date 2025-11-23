9756bf61a77d54505af66162e3e91b1d13f71125
kiiisy <ksy@kisayanoMacBook-Pro.local>
2025-11-23 17:07:35 +0900

    test: gtest用のコードの引数が古かったので修正

--- verification/DcCut/test_dc_cut.cpp
+++ verification/DcCut/test_dc_cut.cpp
@@ -8,22 +8,24 @@ bool run_dc_cut_case(const std::string &case_name, double &rmse_out)
 {
     return run_case_generic(
         "DcCut", case_name,
-        [&](audio::axis_stream_t &s, audio::axis_stream_t &m) {
-            static bool  pass  = false;
+        [&](audio::axis_stream_t &s, audio::axis_stream_t &m)
+        {
+            static bool pass = false;
             static float acoef = std::exp(-2.0f * float(M_PI) * dc_cut_cutoff_hz() / sample_rate());
             dc_cut(s, m, acoef, pass);
         },
         rmse_out);
 }
 
-#ifdef HLS_TB_STANDALONE  // HLSプロジェクトのCFLAGSで定義
+#ifdef HLS_TB_STANDALONE // HLSプロジェクトのCFLAGSで定義
 
 int main()
 {
-    try {
+    try
+    {
         struct CaseInfo
         {
-            uint16_t    index;  // 使わなくていいかも
+            uint16_t index; // 使わなくていいかも
             const char *name;
         };
 
@@ -39,58 +41,64 @@ int main()
         };
 
         bool all_ok = true;
-        for (auto &c : cases) {
+        for (auto &c : cases)
+        {
             double rmse = 0.0;
-            bool   ok   = run_dc_cut_case(c.name, rmse);
-            if (!ok) {
+            bool ok = run_dc_cut_case(c.name, rmse);
+            if (!ok)
+            {
                 std::cerr << "[NG] " << c.name << " RMSE=" << rmse << "\n";
                 all_ok = false;
-            } else {
+            }
+            else
+            {
                 std::cout << "[OK] " << c.name << " RMSE=" << rmse << "\n";
             }
         }
 
         return all_ok ? 0 : 1;
-    } catch (const std::exception &e) {
+    }
+    catch (const std::exception &e)
+    {
         std::cerr << "[EXCEPTION] " << e.what() << "\n";
         return 1;
     }
 }
 
-#endif  // HLS_TB_STANDALONE
+#endif // HLS_TB_STANDALONE
 
-#ifdef USE_GTEST  // CMakeでunit_testsターゲットで定義
+#ifdef USE_GTEST // CMakeでunit_testsターゲットで定義
 
 #include <gtest/gtest.h>
 
 TEST(DcCut, Case1)
 {
     double rmse = 0.0;
-    ASSERT_TRUE(run_dc_cut_case(1, "dc_cut_case1", rmse)) << "RMSE=" << rmse;
+    ASSERT_TRUE(run_dc_cut_case("dc_cut_case1", rmse)) << "RMSE=" << rmse;
 }
 
 TEST(DcCut, Case2)
 {
     double rmse = 0.0;
-    ASSERT_TRUE(run_dc_cut_case(2, "dc_cut_case2", rmse)) << "RMSE=" << rmse;
+    ASSERT_TRUE(run_dc_cut_case("dc_cut_case2", rmse)) << "RMSE=" << rmse;
 }
 
 TEST(DcCut, Case3)
 {
     double rmse = 0.0;
-    ASSERT_TRUE(run_dc_cut_case(3, "dc_cut_case3", rmse)) << "RMSE=" << rmse;
+    ASSERT_TRUE(run_dc_cut_case("dc_cut_case3", rmse)) << "RMSE=" << rmse;
 }
 
 TEST(DcCut, Case4)
 {
     double rmse = 0.0;
-    ASSERT_TRUE(run_dc_cut_case(4, "dc_cut_case4", rmse)) << "RMSE=" << rmse;
+    ASSERT_TRUE(run_dc_cut_case("dc_cut_case4", rmse)) << "RMSE=" << rmse;
 }
 
 TEST(DcCut, Case5)
 {
     double rmse = 0.0;
-    ASSERT_TRUE(run_dc_cut_case(5, "dc_cut_case5", rmse)) << "RMSE=" << rmse;
+    ASSERT_TRUE(run_dc_cut_case("dc_cut_case5", rmse)) << "RMSE=" << rmse;
 }
 
-#endif  // USE_GTEST
+#endif // USE_GTEST
